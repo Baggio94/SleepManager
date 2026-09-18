5eae8038cbb947ea35cda34b4ac33fdabe55baac
Baggio94 <sahbito@gmail.com>
2026-09-18 11:04:17 +0200

    Version helper 0.3.0-dev2

--- helper/build.gradle.kts
+++ helper/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.med.sleepmanager.helper"
         minSdk = 28
         targetSdk = 28
-        versionCode = 300
-        versionName = "0.3.0"
+        versionCode = 301
+        versionName = "0.3.0-dev2"
     }
 
     compileOptions {
