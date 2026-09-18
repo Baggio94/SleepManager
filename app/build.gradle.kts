524d44a21001e7208c0329378fac1da6467a5706
Baggio94 <sahbito@gmail.com>
2026-09-18 11:04:10 +0200

    Version 0.3.0-dev2 test build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.med.sleepmanager"
         minSdk = 28
         targetSdk = 36
-        versionCode = 300
-        versionName = "0.3.0"
+        versionCode = 301
+        versionName = "0.3.0-dev2"
     }
 
     buildFeatures {
