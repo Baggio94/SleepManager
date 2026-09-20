c9c0f109e1f59116a7d5d8e6b0e87a6e2c0fed92
Baggio94 <sahbito@gmail.com>
2026-09-21 01:03:44 +0200

    Add FileProvider dependency for updater

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,6 +91,7 @@ android {
 
 dependencies {
     implementation("androidx.activity:activity-compose:1.12.4")
+    implementation("androidx.core:core:1.17.0")
 
     implementation("androidx.compose.ui:ui:1.11.4")
     implementation("androidx.compose.ui:ui-tooling-preview:1.11.4")
