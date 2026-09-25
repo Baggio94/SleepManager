8836c3b8a1724ecd6d14608919ed3f63d128221f
Baggio94 <sahbito@gmail.com>
2026-09-25 08:35:41 +0200

    Cancel update work when it is no longer needed

--- app/src/main/java/com/med/sleepmanager/update/UpdateCheckJobService.kt
+++ app/src/main/java/com/med/sleepmanager/update/UpdateCheckJobService.kt
@@ -4,20 +4,43 @@ import android.app.job.JobParameters
 import android.app.job.JobService
 
 class UpdateCheckJobService : JobService() {
+    @Volatile
+    private var workerThread: Thread? = null
+
+    @Volatile
+    private var stoppedBySystem = false
+
     override fun onStartJob(params: JobParameters): Boolean {
-        Thread {
-            try {
-                UpdateChecker.check(
-                    context = this,
-                    force = false,
-                    notify = true
-                )
-            } finally {
-                jobFinished(params, false)
-            }
-        }.start()
+        stoppedBySystem = false
+
+        val worker =
+            Thread(
+                {
+                    try {
+                        UpdateChecker.check(
+                            context = this,
+                            force = false,
+                            notify = true
+                        )
+                    } finally {
+                        workerThread = null
+                        if (!stoppedBySystem) {
+                            jobFinished(params, false)
+                        }
+                    }
+                },
+                "SleepManagerUpdateJob"
+            )
+
+        workerThread = worker
+        worker.start()
         return true
     }
 
-    override fun onStopJob(params: JobParameters): Boolean = true
+    override fun onStopJob(params: JobParameters): Boolean {
+        stoppedBySystem = true
+        workerThread?.interrupt()
+        workerThread = null
+        return true
+    }
 }
